97af067d2f49ff33047c52e00d7a9b33584bf3f2
Neon Ratemo <neon.ratemo@student.moringaschool.com>
2023-09-03 12:50:22 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -4,5 +4,42 @@ CONN = sqlite3.connect('lib/dogs.db')
 CURSOR = CONN.cursor()
 
 class Dog:
+
+    def __init__(self, name, breed):
+        self.id = None
+        self.name = name
+        self.breed = breed
     
-    pass
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+        """
+        CURSOR.execute(sql)
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS dogs
+        """
+        CURSOR.execute(sql)
+
+    def save(self):
+        sql = """
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.breed))
+
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM dogs").fetchone()[0]
+
+    @classmethod
+    def create(cls, name, breed):
+        song = Song(name, album)
+        song.save()
+        return song
+
